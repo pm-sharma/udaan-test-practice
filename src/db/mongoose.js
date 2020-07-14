9d5f5722243225f63387ea2e8c245d1c2d7e101b
pm-sharma <pmsharma1973990@gmail.com>
2020-07-14 18:54:54 +0530

    hosting

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/udaan-db-test', {
+// mongoose.connect('mongodb://127.0.0.1:27017/udaan-db-test', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+// })
+
+mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
